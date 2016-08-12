f14c27d2508118b99c95cc81bf792d78a732620d
linkjr <linkjr@sina.com>
2016-08-13 00:11:22 +0800

    Add handler for image.

---
+++ Nbray.Web/Handler/ImgHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Nbray.Web.Handler
+{
+    public class ImgHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.Request.UrlReferrer != null)
+            {
+                foreach (var domain in ConfigurationManager.AppSettings["domain"].Split(','))
+                {
+                    if (context.Request.UrlReferrer.AbsoluteUri.Contains(domain))
+                    {
+                        context.Response.ContentType = "image/gif";
+                        context.Response.WriteFile(context.Request.FilePath);
+                    }
+                    else
+                    {
+                        context.Response.ContentType = "image/jpeg";
+                        context.Response.WriteFile("error.jpg");
+                    }
+                }
+            }
+        }
+    }
+}

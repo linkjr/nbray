69e798f3784d8730054dd540fe93ff023f4d07db
linkjr <linkjr@sina.com>
2016-09-05 15:56:25 +0800

    Migrate to git.

---
+++ Nbray/DisposableObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nbray
+{
+    /// <summary>
+    /// 释放对象的资源。
+    /// </summary>
+    public abstract class DisposableObject : IDisposable
+    {
+        ~DisposableObject()
+        {
+            this.Dispose(false);
+        }
+
+        /// <summary>
+        /// 释放当前对象的所有资源
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// 释放非托管资源和托管资源（后者为可选项）。
+        /// </summary>
+        /// <param name="disposing">若为 <see cref="true"/>，则同时释放托管资源和非托管资源；若为 <see cref="false"/>，则仅释放非托管资源。</param>
+        protected abstract void Dispose(bool disposing);
+    }
+}
---
+++ Nbray/EnumDisplay.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nbray
+{
+    public static class EnumDisplay
+    {
+        /// <summary>
+        /// 返回指定枚举值的描述信息。
+        /// </summary>
+        /// <param name="enumVal"></param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum enumVal)
+        {
+            var val = enumVal.ToString();
+            var typ = enumVal.GetType();
+            var fieldInfo = typ.GetField(enumVal.ToString());
+            string result;
+            if (fieldInfo == null)
+                result = val;
+            else
+            {
+                var objs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (objs == null || objs.Length == 0)
+                {
+                    result = val;
+                }
+                else
+                {
+                    var da = (DescriptionAttribute)objs[0];
+                    result = da.Description;
+                }
+            }
+            return result;
+        }
+    }
+}

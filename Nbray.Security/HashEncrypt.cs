22a75fc2838e14cc3b0d21747aae059cdf18bffd
linkjr <linkjr@sina.com>
2016-09-05 15:01:46 +0800

    优化HASH加密写法。

--- Nbray.Security/HashEncrypt.cs
+++ Nbray.Security/HashEncrypt.cs
@@ -15,7 +15,7 @@ namespace Nbray.Security
     public class HashEncrypt
     {
         /// <summary>
-        /// 加密字符串文本
+        /// 【已过时】加密字符串文本
         /// </summary>
         /// <param name="plaintext">要加密的字符串</param>
         /// <param name="passwordFormat">加密方式</param>
@@ -27,7 +27,7 @@ namespace Nbray.Security
         }
 
         /// <summary>
-        /// 根据MD5加密字符串文本
+        /// 【已过时】根据MD5加密字符串文本。
         /// </summary>
         /// <param name="plaintext">要加密的字符串</param>
         /// <returns></returns>
@@ -38,29 +38,44 @@ namespace Nbray.Security
         }
 
         /// <summary>
+        /// MD5哈希加密字节数组。
+        /// </summary>
+        /// <param name="bytes">要加密的字节数组。</param>
+        /// <returns>返回MD5加密后的哈希值。</returns>
+        public static string MD5Hash(byte[] bytes)
+        {
+            var hash = new StringBuilder();
+            var algorithm = new MD5CryptoServiceProvider(); //replace MD5.Create()
+            //algorithm.Hash as same as the value execute algorithm.ComputeHash(bytes);
+            algorithm.ComputeHash(bytes)
+                .ToList()
+                .ForEach(m => hash.AppendFormat("{0:X2}", m));  //{0:x2} hex string, replace the method of BitConverter.ToString.
+            var hexString = hash.ToString();
+            return hexString;
+        }
+
+        /// <summary>
         /// MD5哈希加密字符串。
         /// </summary>
-        /// <param name="inputText">要加密的文本。</param>
+        /// <param name="text">要加密的文本。</param>
         /// <returns>返回MD5加密后的哈希值。</returns>
-        public static string MD5Hash(string inputText)
+        public static string MD5Hash(string text)
         {
-            //var algorithm = MD5.Create();
-            var algorithm = new MD5CryptoServiceProvider();
-            var bytes = algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputText));
-            var hexString = BitConverter.ToString(algorithm.Hash).Replace("-", string.Empty).ToUpper();
+            var bytes = Encoding.UTF8.GetBytes(text);
+            var hexString = MD5Hash(bytes);
             return hexString;
         }
 
         /// <summary>
         /// SHA1哈希加密字符串。
         /// </summary>
-        /// <param name="inputText">要加密的文本。</param>
+        /// <param name="text">要加密的文本。</param>
         /// <returns>返回SHA1加密后的哈希值。</returns>
-        public static string SHA1Hash(string inputText)
+        public static string SHA1Hash(string text)
         {
             //var algorithm = SHA1.Create();
             var algorithm = new SHA1CryptoServiceProvider();
-            var bytes = algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputText));
+            var bytes = algorithm.ComputeHash(Encoding.UTF8.GetBytes(text));
             var hexString = BitConverter.ToString(algorithm.Hash).Replace("-", string.Empty).ToUpper();
             return hexString;
         }

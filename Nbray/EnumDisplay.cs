7696e8631e4c05431490e0a2c8bc31480cc40317
linkjr <linkjr@sina.com>
2016-09-11 22:28:12 +0800

    添加获取指定枚举类型的项的方法。

--- Nbray/EnumDisplay.cs
+++ Nbray/EnumDisplay.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,6 +37,34 @@ namespace Nbray
                 }
             }
             return result;
+        }
+
+        /// <summary>
+        /// 根据指定枚举类型返回对应的值和描述组成的字典。
+        /// </summary>
+        /// <param name="enumType">指定的枚举类型。</param>
+        /// <returns></returns>
+        public static Dictionary<int, string> GetEnumOptions(Type enumType)
+        {
+            var list = new Dictionary<int, string>();
+            var fields = enumType.GetFields();
+            for (int i = 0; i < fields.Length; i++)
+            {
+                var fieldInfo = fields[i];
+                if (fieldInfo.FieldType.BaseType != typeof(Enum))
+                    continue;
+
+                var value = fieldInfo.Name;
+                var objs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (objs != null && objs.Length != 0)
+                {
+                    var da = (DescriptionAttribute)objs[0];
+                    value = da.Description;
+                }
+                var key = (int)Enum.Parse(enumType, fieldInfo.Name);
+                list.Add(key, value);
+            }
+            return list;
         }
     }
 }

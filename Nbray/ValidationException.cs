8ca97011896bcac781572c8c738fb5a68355d1ba
linkjr <linkjr@sina.com>
2016-09-11 22:53:40 +0800

    新增验证异常类。

---
+++ Nbray/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nbray
+{
+    /// <summary>
+    /// 表示验证异常类。
+    /// </summary>
+    public class ValidationException : Exception
+    {
+        /// <summary>
+        /// 初始化 <c>ValidationException</c> 类的新实例。
+        /// </summary>
+        /// <param name="message"></param>
+        public ValidationException(string message) : base(message) { }
+    }
+}
